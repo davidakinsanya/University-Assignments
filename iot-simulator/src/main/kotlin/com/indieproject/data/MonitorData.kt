6748e43fca66e6766f78cba0356c1fa289dbecb7
David Akinsanya <davidakinsanya00@gmail.com>
2021-11-10 20:47:18 +0000

    toString() method added to the MonitorData class so the structure can be tested for randomness.

--- iot-simulator/src/main/kotlin/com/indieproject/data/MonitorData.kt
+++ iot-simulator/src/main/kotlin/com/indieproject/data/MonitorData.kt
@@ -7,10 +7,8 @@ data class MonitorData(
                        private val OxygenSaturation: Int,
                        private val Respiration: Int,
                        private val Temperature: Int,
-                       )
+                       ) {
 
-/*
-{
   fun getMonitorNumber(): Int {
   return MonitorNumber
   }
@@ -34,7 +32,15 @@ data class MonitorData(
   fun getTemperature(): Int {
     return Temperature
   }
+  
+  override fun toString(): String {
+    return "Monitor #${this.getMonitorNumber()} reads: " +
+            "\nPulse: ${this.getPulse()}" +
+            "\nBlood Pressure: ${this.getBloodPressure()[0]}/${this.getBloodPressure()[1]}" +
+            "\nOxygen Saturation: ${this.getOxygenSaturation()}" +
+            "\n Resipration: ${this.getRespiration()}" +
+            "\n Temperature: ${this.getTemperature()}"
+  }
+  
 }
-
-*/
 
