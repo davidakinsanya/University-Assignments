931aed87c7a2810928e9699529ba3f7c0aa30fe3
David Akinsanya <davidakinsanya00@gmail.com>
2021-11-10 22:32:08 +0000

    code cleanup, exception arose whihc needs to be handled on the server side.

--- iot-simulator/src/main/kotlin/com/indieproject/routes/MonitorRoutes.kt
+++ iot-simulator/src/main/kotlin/com/indieproject/routes/MonitorRoutes.kt
@@ -1,16 +1,13 @@
 package com.indieproject.routes
 
-import com.indieproject.data.MonitorData
 import com.indieproject.utils.MonitorUtils
 import io.ktor.application.*
-import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
 
 fun Application.monitorRoutes() {
   routing {
     post("/monitor") {
-      // val data = call.receive<MonitorData>()
       val utils = MonitorUtils()
       call.respond(utils.generateMonitorObject())
       call.respondText("Done!")
