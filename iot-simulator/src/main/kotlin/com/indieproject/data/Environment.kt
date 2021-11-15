b34fec2956ea720af34bd187d3f36335e9fb39ca
David <davidakinsanya00@gmail.com>
2021-11-15 23:01:46 +0000

    Extending the environment enum to include different hospital environments.

--- iot-simulator/src/main/kotlin/com/indieproject/data/Environment.kt
+++ iot-simulator/src/main/kotlin/com/indieproject/data/Environment.kt
@@ -1,23 +1,8 @@
 package com.indieproject.data
 
-import com.indieproject.utils.EnvironmentUtils
 import kotlinx.serialization.Serializable
 
 @Serializable
-enum class Environment(private val identifier: Int, private val status: Status) {
-  VACCINE_STORAGE(EnvironmentUtils().setIdentifier(), EnvironmentUtils().setStatus());
-  
-  fun getIdentifier(): Int {
-    return identifier
-  }
-  
-  fun getStatus(): Status {
-    return status
-  }
-  
-  override fun toString(): String {
-    return "$this #${this.getIdentifier()} " +
-            "Condition reads as ${this.getStatus()} " +
-            "(${this.getStatus().getRate()}/10)"
-  }
+enum class Environment() {
+  PHARMACEUTICAL_STORAGE, PATIENT_WARD, INCUBATOR, LAVATORY;
 }
