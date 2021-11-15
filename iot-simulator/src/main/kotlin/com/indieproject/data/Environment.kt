7c6a4c3126f70391f21e2e13b0534c06b81fda85
David <davidakinsanya00@gmail.com>
2021-11-15 21:52:23 +0000

    Coding up the getter methods for all new enum classes.

--- iot-simulator/src/main/kotlin/com/indieproject/data/Environment.kt
+++ iot-simulator/src/main/kotlin/com/indieproject/data/Environment.kt
@@ -2,6 +2,14 @@ package com.indieproject.data
 
 import com.indieproject.utils.EnvironmentUtils
 
-enum class Environment(val identifier: Int, val status: Status) {
-  VACCINE_STORAGE(EnvironmentUtils().setIdentifier(), EnvironmentUtils().setStatus())
+enum class Environment(private val identifier: Int, private val status: Status) {
+  VACCINE_STORAGE(EnvironmentUtils().setIdentifier(), EnvironmentUtils().setStatus());
+  
+  fun getIdentifier(): Int {
+    return identifier
+  }
+  
+  fun getStatus(): Status {
+    return status
+  }
 }
--- iot-simulator/src/main/kotlin/com/indieproject/data/Status.kt
+++ iot-simulator/src/main/kotlin/com/indieproject/data/Status.kt
@@ -2,9 +2,13 @@ package com.indieproject.data
 
 import com.indieproject.utils.StatusUtils
 
-enum class Status(val rate: Int) {
+enum class Status(private val rate: Int) {
   SUB_OPTIMAL(StatusUtils().setRate()),
   MODERATE(StatusUtils().setRate()),
   STANDARD(StatusUtils().setRate()),
-  OPTIMAL(StatusUtils().setRate())
+  OPTIMAL(StatusUtils().setRate());
+  
+  fun getRate(): Int {
+    return rate
+  }
 }
