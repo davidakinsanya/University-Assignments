4f8b4148f41c9e491128f5c20b30d3170218a247
David <davidakinsanya00@gmail.com>
2021-11-17 20:31:41 +0000

    Beginning documentation of the classes in the utils package of the rest api.

--- iot-simulator/src/main/kotlin/com/indieproject/utils/EnvironmentUtils.kt
+++ iot-simulator/src/main/kotlin/com/indieproject/utils/EnvironmentUtils.kt
@@ -5,11 +5,27 @@ import com.indieproject.data.EnvironmentData
 import com.indieproject.data.Status
 import kotlin.random.Random
 
+/**
+ * This class is tasked with feeding data to
+ * and generating EnvironmentData objects.
+ *
+ * @author David
+ */
 class EnvironmentUtils {
+  
+  /**
+   * This method generates a random integer
+   * to be assigned as an environment identifier.
+   *
+   * @return a random integer between 900 and 3000.
+   */
   fun setIdentifier(): Int {
     return Random.nextInt(900, 3000)
   }
   
+  /**
+   * 
+   */
    fun setStatus(): Status {
     return Status.values().random()
   }
