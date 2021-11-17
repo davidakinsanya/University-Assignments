996e1de0877cbbcacad2003ed15179ee854c7015
David <davidakinsanya00@gmail.com>
2021-11-17 20:25:08 +0000

    Concluding the documentation of all the current data classes in the data package of the rest api.

--- iot-simulator/src/main/kotlin/com/indieproject/data/Environment.kt
+++ iot-simulator/src/main/kotlin/com/indieproject/data/Environment.kt
@@ -2,6 +2,11 @@ package com.indieproject.data
 
 import kotlinx.serialization.Serializable
 
+/**
+ * This class models the different static environments in generic hospitals.
+ *
+ * @author David
+ */
 @Serializable
 enum class Environment() {
   PHARMACEUTICAL_STORAGE, PATIENT_WARD, INCUBATOR, LAVATORY;
--- iot-simulator/src/main/kotlin/com/indieproject/data/Status.kt
+++ iot-simulator/src/main/kotlin/com/indieproject/data/Status.kt
@@ -3,6 +3,14 @@ package com.indieproject.data
 import com.indieproject.utils.StatusUtils
 import kotlinx.serialization.Serializable
 
+/**
+ * This class models the static categories of
+ * standardizing conditions in hospital environments.
+ *
+ *
+ * @param rate the out of 10 rating within each static category.
+ * @author David
+ */
 @Serializable
 enum class Status(private val rate: Int) {
   SUB_OPTIMAL(StatusUtils().setRate()),
@@ -10,6 +18,14 @@ enum class Status(private val rate: Int) {
   STANDARD(StatusUtils().setRate()),
   OPTIMAL(StatusUtils().setRate());
   
+  /**
+   * This method retrieves the out of 10 rating
+   * within each static category.
+   *
+   *
+   * @return the out of 10 rating
+   * within each static category.
+   */
   fun getRate(): Int {
     return rate
   }
