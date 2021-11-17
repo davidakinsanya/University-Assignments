4c7e9198de5225b0e802895e62ff2fa9c89b89a1
David <davidakinsanya00@gmail.com>
2021-11-17 20:04:22 +0000

    Beginning the process of writing documentation for the rest api.

--- iot-simulator/src/main/kotlin/com/indieproject/data/MonitorData.kt
+++ iot-simulator/src/main/kotlin/com/indieproject/data/MonitorData.kt
@@ -2,6 +2,20 @@ package com.indieproject.data
 
 import kotlinx.serialization.Serializable
 
+/**
+ *  This class models the data displayed on
+ *  a vitals monitor for
+ *  hospital patients.
+ *
+ *
+ *  @param MonitorNumber A monitor number from the simulated IOT device.
+ *  @param Pulse A pulse reading from the simulated IOT device.
+ *  @param BloodPressure A blood pressure reading from the simulated IOT device.
+ *  @param OxygenSaturation An oxygen saturation reading from the simulated IOT device.
+ *  @param Respiration A respiration rate reading from the simulated IOT device.
+ *  @param Temperature A temperature reading from the simulated IOT device.
+ *  @author David
+ */
 @Serializable
 data class MonitorData(
                        private val MonitorNumber: Int,
@@ -11,31 +25,92 @@ data class MonitorData(
                        private val Respiration: Int,
                        private val Temperature: Double,
                        ) {
-
+  
+  /**
+   * This method retrieves the pulse reading from the
+   * MonitorData object sent from the simulated IOT device.
+   *
+   *
+   * @return the monitor number from the
+   * MonitorData object sent from the
+   * simulated IOT device.
+   */
   private fun getMonitorNumber(): Int {
   return MonitorNumber
   }
-  
+  /**
+   * This method retrieves the pulse reading from the
+   * MonitorData object sent from the simulated IOT device.
+   *
+   *
+   * @return the pulse reading from the
+   * MonitorData object sent from the
+   * simulated IOT device.
+   */
   private fun getPulse(): Int {
     return Pulse
   }
-
+  /**
+   * This method retrieves the blood pressure reading from the
+   * MonitorData object sent from the simulated IOT device.
+   *
+   *
+   * @return the blood pressure reading from the
+   * MonitorData object sent from the
+   * simulated IOT device.
+   */
   private fun getBloodPressure(): Array<Int> {
     return BloodPressure
   }
   
+  /**
+   * This method retrieves the oxygen saturation reading from the
+   * MonitorData object sent from the simulated IOT device.
+   *
+   *
+   * @return the oxygen saturation from the
+   * MonitorData object sent from the
+   * simulated IOT device.
+   */
   private fun getOxygenSaturation(): Int {
     return OxygenSaturation
   }
   
+  /**
+   * This method retrieves the respiration rate reading
+   * from the MonitorData object sent
+   * from the simulated IOT device.
+   *
+   *
+   * @return the respiration rate from the
+   * MonitorData object sent from the
+   * simulated IOT device.
+   */
   private fun getRespiration(): Int {
     return Respiration
   }
   
+  /**
+   * This method retrieves the temperature reading from the
+   * MonitorData object sent from the simulated IOT device.
+   *
+   *
+   * @return the temperature reading from the
+   * MonitorData object sent from the
+   * simulated IOT device.
+   */
   private fun getTemperature(): Double {
     return Temperature
   }
   
+  /**
+   * This method provides
+   * a string representation of the data points
+   * in the MonitorData class.
+   *
+   * @return a string representation of the data points
+   * in the MonitorData class.
+   */
   override fun toString(): String {
     return "Monitor #${this.getMonitorNumber()} reads: " +
             "\n\nPulse: ${this.getPulse()} Beats Per Minute" +
