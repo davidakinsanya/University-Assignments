34b9fa5760c6bf1db0714aedb2a5cde18b8a60a8
David <davidakinsanya00@gmail.com>
2021-12-22 21:46:23 +0000

    adding a default path to the server for testing

--- iot-simulator/src/main/kotlin/com/indieproject/plugins/Routing.kt
+++ iot-simulator/src/main/kotlin/com/indieproject/plugins/Routing.kt
@@ -4,11 +4,16 @@ import com.indieproject.routes.environmentRoutes
 import com.indieproject.routes.monitorRoutes
 import io.ktor.routing.*
 import io.ktor.application.*
+import io.ktor.response.*
 
 fun Application.configureRouting() {
 
     routing {
       monitorRoutes()
       environmentRoutes()
+      
+      get("/") {
+        call.respond("Hello Ktor!")
+      }
     }
 }
