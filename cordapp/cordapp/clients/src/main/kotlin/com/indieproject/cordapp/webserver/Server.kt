f771122288098bc4e4bf2be776d9e69af90f60a4
David <davidakinsanya00@gmail.com>
2022-01-05 13:35:31 +0000

    unused imports

--- cordapp/cordapp/clients/src/main/kotlin/com/indieproject/cordapp/webserver/Server.kt
+++ cordapp/cordapp/clients/src/main/kotlin/com/indieproject/cordapp/webserver/Server.kt
@@ -1,22 +1,8 @@
 package com.indieproject.cordapp.webserver
 
-import com.fasterxml.jackson.core.util.DefaultIndenter
-import com.fasterxml.jackson.core.util.DefaultPrettyPrinter
-import com.fasterxml.jackson.databind.ObjectMapper
-import io.ktor.application.*
-import io.ktor.features.*
-import io.ktor.http.*
-import io.ktor.jackson.*
-import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import net.corda.client.jackson.JacksonSupport
-import net.corda.client.rpc.CordaRPCClient
-import net.corda.client.rpc.CordaRPCConnection
-import net.corda.core.messaging.CordaRPCOps
-import net.corda.core.utilities.NetworkHostAndPort
-import org.slf4j.event.Level
-import java.util.concurrent.TimeUnit
+
 
 
 fun main(args: Array<String>) {
