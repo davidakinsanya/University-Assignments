25680f92c8660ec991573d3ff7c2d808b0296636
David <davidakinsanya00@gmail.com>
2022-01-05 11:36:10 +0000

    Bug issue fixed with a gradle task found on github.

--- cordapp/cordapp/clients/src/main/kotlin/com/indieproject/cordapp/webserver/Routing.kt
+++ cordapp/cordapp/clients/src/main/kotlin/com/indieproject/cordapp/webserver/Routing.kt
@@ -1,11 +1,14 @@
 package com.indieproject.cordapp.webserver
 
 import io.ktor.application.*
+import io.ktor.response.*
 import io.ktor.routing.*
 
 fun Application.msgRouting() {
- routing {
-  // post {  }
- }
+  routing {
+    post("/msg/{newMsg}") {
+      call.respond("Hello World")
+    }
+  }
 }
 
