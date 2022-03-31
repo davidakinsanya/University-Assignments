3e26d12f093cf6b28b3193ae5e512a8b2a847544
David <davidakinsanya00@gmail.com>
2022-03-31 03:16:25 +0100

    completing documentation for the server side.

--- client/app/src/main/java/com/indieproject/client/repository/EnvRepository.kt
+++ client/app/src/main/java/com/indieproject/client/repository/EnvRepository.kt
@@ -10,14 +10,30 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
+/**
+ * This class controls the handling of Environment data on the client side.
+ *
+ * @author david.
+ */
 class EnvRepository {
 
+  /**
+   * This function creates a log message using the appropriate methods in the EnvMsg class.
+   *
+   * @param env an EnvironmentData object.
+   * @param db a DbHelper object, used to interact with an sql local database.
+   */
   fun generateLogMessage(env: EnvironmentData?, db: DbHelper) {
     val createMsg = EnvMsg(env)
     val msg = createMsg.generateLogMessage(createMsg.evalEnvironmentLog())
     pushLogMessage(env!!, msg, db)
   }
 
+  /**
+   * This function initiates the process of logging data to the blockchain.
+   *
+   * @param db a DbHelper object.
+   */
    fun logData(db: DbHelper) {
      RetrofitInstance.env.getData().enqueue(object : Callback<EnvironmentData?> {
        override fun onResponse(call: Call<EnvironmentData?>, response: Response<EnvironmentData?>) {
@@ -31,6 +47,14 @@ class EnvRepository {
      })
    }
 
+  /**
+   * This method pushes the log message to the blockchain
+   * and adds it to the local database.
+   *
+   * @param env a EnvironmentData object.
+   * @param db a DbHelper Object.
+   * @param newMsg the log message produced.
+   */
   private fun pushLogMessage(env: EnvironmentData, newMsg: String, db: DbHelper) {
     RetrofitInstance.envTwo.pushLogMessage(newMsg).enqueue(object : Callback<String?> {
       override fun onResponse(call: Call<String?>, response: Response<String?>) {
--- client/app/src/main/java/com/indieproject/client/repository/MonitorRepository.kt
+++ client/app/src/main/java/com/indieproject/client/repository/MonitorRepository.kt
@@ -10,15 +10,30 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-
+/**
+ * This class controls the handling of Monitor data on the client side.
+ *
+ * @author david.
+ */
 class MonitorRepository() {
 
+  /**
+   * This function creates a log message using the appropriate methods in the MonitorMsg class.
+   *
+   * @param mon an MonitorData object.
+   * @param db a DbHelper object, used to interact with an sql local database.
+   */
   fun generateLogMessage(mon: MonitorData?, db: DbHelper) {
     val createMsg = MonitorMsg(mon)
     val msg = createMsg.generateLogMessage(createMsg.getLogList())
     pushLogMessage(mon!!, db, msg, createMsg.getDangerStatus())
   }
 
+  /**
+   * This function initiates the process of logging data to the blockchain.
+   *
+   * @param db a DbHelper object.
+   */
   fun logData(db: DbHelper) {
     RetrofitInstance.mon.getData().enqueue(object : Callback<MonitorData?> {
       override fun onResponse(call: Call<MonitorData?>, response: Response<MonitorData?>) {
@@ -31,6 +46,15 @@ class MonitorRepository() {
     })
   }
 
+  /**
+   * This method pushes the log message to the blockchain
+   * and adds it to the local database.
+   *
+   * @param mon a MonitorData object.
+   * @param db a DbHelper Object.
+   * @param newMsg the log message produced.
+   * @param danger a boolean value indicating if the patient could be in danger.
+   */
   private fun pushLogMessage(mon: MonitorData, db: DbHelper, newMsg: String, danger: Boolean) {
     RetrofitInstance.monTwo.pushLogMessage(newMsg).enqueue(object : Callback<String?> {
       override fun onResponse(call: Call<String?>, response: Response<String?>) {
